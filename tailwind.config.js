3108ae4b42edfe9f072c8b1d7d1e92be683d20fe
repoleved08 <geekbii08@gmail.com>
2024-09-14 01:34:05 +0300

    added css animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,13 @@ module.exports = {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
+        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554", 'neon-green': '#39FF14',
+          'neon-blue': '#1F51FF',
+          'neon-purple': '#D500F9',
+          'dark-bg': '#1F1F1F',
+          'dark-text': '#E0E0E0',
+          'light-bg': '#F5F5F5',
+          'light-text': '#333333',}
       }
     },
   },

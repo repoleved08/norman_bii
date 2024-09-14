d93c9dd34e8fc67818fd3b9ed1dd8b3f17af8b16
repoleved08 <geekbii08@gmail.com>
2024-09-14 10:24:16 +0300

    added assets

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 import 'flowbite';
-// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+
 if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     document.documentElement.classList.add('dark')
   } else {
@@ -12,23 +12,31 @@ if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.match
   // Whenever the user explicitly chooses dark mode
   localStorage.theme = 'dark'
   
-  // Whenever the user explicitly chooses to respect the OS preference
   localStorage.removeItem('theme')
 
-  // Togling navbar 
+  // toggle nav
+
   document.addEventListener("DOMContentLoaded", function () {
     const toggleButton = document.querySelector("[data-collapse-toggle]");
     const menu = document.getElementById("mobile-menu-2");
-
+    const navbar = document.getElementById("main-navbar");
+  
     toggleButton.addEventListener("click", function () {
-      // Toggle the 'hidden' class
       menu.classList.toggle("hidden");
-
-      // Optionally, toggle the visibility of the icon (hamburger and close icons)
+  
+    
+      if (!menu.classList.contains("hidden")) {
+        navbar.classList.remove("rounded-full");
+      } else {
+        navbar.classList.add("rounded-full");
+      }
+  
+      // Optionally, toggle the visibility of the hamburger/close icons
       const iconOpen = toggleButton.querySelector("svg:first-child");
       const iconClose = toggleButton.querySelector("svg:last-child");
-
+  
       iconOpen.classList.toggle("hidden");
       iconClose.classList.toggle("hidden");
     });
   });
+  
